5ab8bbcdb3381cb19ac2acf02c80bab6a5b50873
2-0-0-4 <afeeraumair@gmail.com>
2025-04-07 11:47:20 +0500

    data_sets

---
+++ Block.hpp
@@ -0,0 +1,5 @@
+#ifndef BLOCK_HPP
+
+
+
+#endif
--- MerkleTree.hpp
+++ MerkleTree.hpp
@@ -1,8 +1,23 @@
+#ifndef MERKLE_TREE_HPP
+#define MERKLE_TREE_HPP
+
+#include <vector>
+struct Node
+{
+    Node* parent;
+    
+};
+
 class MerkleTree
 {
 private:
-    /* data */
+    std::vector<Node> leaf_nodes;
+    
 public:
     MerkleTree(/* args */);
+    void create_tree();
+    void check_if_in_tree();
     ~MerkleTree();
 };
+
+#endif
