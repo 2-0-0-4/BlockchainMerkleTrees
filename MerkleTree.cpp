d96c71aa36eb574cecf342b6cb8453b1f0aab1c0
2-0-0-4 <afeeraumair@gmail.com>
2025-04-08 20:03:00 +0500

    stuff

--- MerkleTree.cpp
+++ MerkleTree.cpp
@@ -1,10 +1,27 @@
 #include "MerkleTree.hpp"
 
+MerkleTree::MerkleTree(){}
 
-MerkleTree::MerkleTree(/* args */)
+MerkleTree::MerkleTree(std::vector<Node> nodes)
 {
+    for(int i = 0; i<nodes.size();i++){
+        leaf_nodes[i] = nodes[i];
+    }
 }
 
-MerkleTree::~MerkleTree()
+Node *MerkleTree::Create_Tree()
 {
+    return nullptr;
 }
+
+bool MerkleTree::in_tree(int index, std::string data)
+{
+    return false;
+}
+
+std::string MerkleTree::getRootHash()
+{
+    return std::string();
+}
+
+MerkleTree::~MerkleTree(){}
--- MerkleTree.hpp
+++ MerkleTree.hpp
@@ -2,9 +2,11 @@
 #define MERKLE_TREE_HPP
 
 #include <vector>
+#include <string>
 struct Node
 {
     Node* parent;
+    std::string hash_value;
     
 };
 
@@ -12,11 +14,14 @@ class MerkleTree
 {
 private:
     std::vector<Node> leaf_nodes;
+    std::string root_hash;
     
 public:
-    MerkleTree(/* args */);
-    void create_tree();
-    void check_if_in_tree();
+    MerkleTree(); //ctor
+    MerkleTree(std::vector<Node> nodes); //parametrized ctor
+    Node* Create_Tree(); //creates tree and returns root 
+    bool in_tree(int index, std::string data); //
+    std::string getRootHash();
     ~MerkleTree();
 };
 
