a1d73c2a0be5b22761346631c4ac0af8287a7ffb
2-0-0-4 <afeeraumair@gmail.com>
2025-04-12 11:26:23 +0500

    merkle delete,proof

--- MerkleTree.cpp
+++ MerkleTree.cpp
@@ -13,49 +13,11 @@ MerkleTree::MerkleTree(std::vector<std::string> dataBlocks)
     this->root_pointer = nullptr;
 }
 
-MerkleTree::~MerkleTree()
-{
+MerkleTree::~MerkleTree(){
+    // delete root_pointer;
+    // delete[] leaf_nodes;
 }
 
-// Node* MerkleTree::create_tree(std::vector<std::string> dataBlocks){
-
-//     //list of leafnodes with the hashvalues
-//      for(const std::string &data : dataBlocks){
-//         Node* temp = new Node(sha256(data));
-//         current_level.push_back(temp);
-//         leaf_nodes.push_back(temp);
-
-//     }
-
-//     //while the vector does not become 1(only root left)
-//     while(current_level.size()>1){
-
-//         std::vector<Node*> next_level;
-
-//         for(int i= 0; i < current_level.size(); i= i+2){
-
-//             if(current_level.size()>i+1){
-//                 next_level.push_back(new Node(sha256(current_level[i]->hashval + current_level[i+1]->hashval))); //hash the combined hashvalues of i and i+1
-//                 //make parent
-//                 current_level[i]->parent = next_level.back();
-//                 current_level[i+1]->parent = next_level.back();
-
-//             }else{
-//                 next_level.push_back(new Node(sha256(current_level[i]->hashval + current_level[i]->hashval)));  //if i+1 not available, combine i+i
-//                 //make parent
-//                 current_level[i]->parent = next_level.back();
-//             }
-//         }
-
-//         current_level = next_level; //move to the upper level
-
-//     }
-
-//     return current_level.front(); //return pointer to the root of the merkle tree
-
-// }
-
-// recursive fucntion
 Node *MerkleTree::create_tree(std::vector<Node *> current_level)
 {
 
@@ -108,3 +70,95 @@ Node *MerkleTree::getRootPointer()
 
     return root_pointer;
 }
+
+bool MerkleTree::Verify_Node(int txn_num, std::string hash_value)
+{
+    std::vector<std::string> path = get_proof(txn_num);
+    std::string curr_hash = sha256(hash_value + path[0]);
+    for (int i = 1; i < path.size(); i++)
+    {
+        curr_hash = sha256(curr_hash + path[i]);
+    }
+    return (curr_hash == root_hash) ? true : false;
+}
+
+
+void MerkleTree::Remove_Node(int i)
+{
+    delete leaf_nodes[i]; //delete the pointer at i
+    leaf_nodes.erase(leaf_nodes.begin() + i); //delete ith pos of vector
+    Create_Tree(); //update hashes
+}
+
+
+std::vector<std::string> MerkleTree::get_proof(int i)
+{
+    Node *curr_node = leaf_nodes[i];
+    std::vector<std::string> path;
+    while (curr_node != root_pointer)
+    {
+        Node *parent_node = curr_node->parent;
+        if (parent_node->left == curr_node)
+        {
+            path.push_back(parent_node->right->hashval);
+        }
+        else
+        {
+            path.push_back(parent_node->left->hashval);
+        }
+        curr_node = parent_node;
+    }
+    return path;
+}
+
+// MerkleTree::~MerkleTree()
+// {
+//     // for (Node *node : leaf_nodes)
+//     // {
+//     //     delete node;
+//     // }
+//     for(int i = 0; i<leaf_nodes.size();i++){
+//         delete leaf_nodes[i];
+//     }
+//     delete root_pointer;
+// }
+
+// Node* MerkleTree::create_tree(std::vector<std::string> dataBlocks){
+
+//     //list of leafnodes with the hashvalues
+//      for(const std::string &data : dataBlocks){
+//         Node* temp = new Node(sha256(data));
+//         current_level.push_back(temp);
+//         leaf_nodes.push_back(temp);
+
+//     }
+
+//     //while the vector does not become 1(only root left)
+//     while(current_level.size()>1){
+
+//         std::vector<Node*> next_level;
+
+//         for(int i= 0; i < current_level.size(); i= i+2){
+
+//             if(current_level.size()>i+1){
+//                 next_level.push_back(new Node(sha256(current_level[i]->hashval + current_level[i+1]->hashval))); //hash the combined hashvalues of i and i+1
+//                 //make parent
+//                 current_level[i]->parent = next_level.back();
+//                 current_level[i+1]->parent = next_level.back();
+
+//             }else{
+//                 next_level.push_back(new Node(sha256(current_level[i]->hashval + current_level[i]->hashval)));  //if i+1 not available, combine i+i
+//                 //make parent
+//                 current_level[i]->parent = next_level.back();
+//             }
+//         }
+
+//         current_level = next_level; //move to the upper level
+
+//     }
+
+//     return current_level.front(); //return pointer to the root of the merkle tree
+
+// }
+
+// recursive fucntion
--- MerkleTree.hpp
+++ MerkleTree.hpp
@@ -6,32 +6,30 @@
 
 struct Node
 {
-    Node* parent;
+    Node *parent;
+    Node *right;
+    Node *left;
     std::string hashval;
-    Node(std::string hashval): hashval(hashval), parent(nullptr) {}
-    
+    Node(std::string hashval) : hashval(hashval), parent(nullptr), right(nullptr), left(nullptr) {}
 };
 
 class MerkleTree
 {
 private:
-    // std::vector<Node*> current_level;
-    std::vector<Node*> leaf_nodes;
-    Node* root_pointer;
+    std::vector<Node *> leaf_nodes;
+    Node *root_pointer;
     std::string root_hash;
-    // Node* MerkleTree::create_tree(std::vector<Node*> leaf_nodes);
-    Node* create_tree(std::vector<Node*> leaf_nodes);
- //recursive definition
-    // Node* create_tree(std::vector<std::string> dataBlocks); //datablocks obtained from csv files , creates tree and returns root
-    // MerkleTree(); //ctor
-    // MerkleTree(std::vector<std::string> dataBlocks); //parametrized ctor
+    Node *create_tree(std::vector<Node *> current_level);
+    std::vector<std::string> get_proof(int i);
+
 public:
-    MerkleTree(); //ctor
-    MerkleTree(std::vector<std::string> dataBlocks); //parametrized ctor
-    void Create_Tree(); //creates tree and returns root 
-    bool in_tree(int index, std::string data); //(if we are going through the tree should we declare left and right pointers also?)
+    MerkleTree();                                    // ctor
+    MerkleTree(std::vector<std::string> dataBlocks); // parametrized ctor
+    void Create_Tree();                              // creates tree and returns root
     std::string getRootHash();
-    Node* getRootPointer();
+    Node *getRootPointer();
+    bool Verify_Node(int txn_num, std::string hash_value);
+    void Remove_Node(int i);
     ~MerkleTree();
 };
 
